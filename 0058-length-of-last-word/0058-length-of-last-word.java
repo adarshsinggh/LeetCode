e56326f675bdbd21e7e8433f766291895794f691
Adarsh Singh <adarshsinggh@gmail.com>
2023-11-13 19:00:54 +0530

    Time: 0 ms (100.00%), Space: 40.8 MB (25.00%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+        String str = s.trim();
+        int count = 0;
+        for(int i=str.length()-1;i>=0;i--){
+            if(str.charAt(i) != ' '){
+                count++;
+            }else{
+                break;
+            }
+        } 
+        return count;
+    }
+}
